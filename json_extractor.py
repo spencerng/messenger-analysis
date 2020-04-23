7f8cac0b64335ccd8363a9eaa4bec6a421504edc
spencerng <sng1488@gmail.com>
2020-04-23 02:52:43 -0400

    Update and reformat scripts

--- json_extractor.py
+++ json_extractor.py
@@ -2,13 +2,24 @@ import json
 import os
 from shutil import copyfile
 
-os.mkdir('json')
-for root, dirs, files in os.walk('.'):
-    for name in files:
-        if name.endswith((".json")):
-            src_file_path = os.path.join(root,name)
-            convo_name = root.split('\\')[-1]
-            des_file_path = '.\\json\\' + convo_name + '.json'
-            
+
+def main():
+    try:
+        os.mkdir("json")
+    except FileExistsError:
+        pass
+
+    for root, dirs, files in os.walk("."):
+        for name in files:
+            if not name.endswith(".json"):
+                continue
+            src_file_path = os.path.join(root, name)
+            convo_name = root.split("\\")[-1]
+
+            des_file_path = ".\\json\\" + convo_name + name.split("_")[1]
 
             copyfile(src_file_path, des_file_path)
+
+
+if __name__ == "__main__":
+    main()
--- json_parser.py
+++ json_parser.py
@@ -2,60 +2,75 @@ import json
 import os
 from shutil import copyfile
 
-messages = []
-for root, dirs, files in os.walk('./json'):
-    for name in files:
-        if name.endswith((".json")):
-            src_file_path = os.path.join(root,name)
-            convo_name = root.split('\\')[-1]
 
-            with open(src_file_path) as json_file:  
+def create_message(fb_m, title, participants):
+    message = dict()
+    message["title"] = title
+    message["participants"] = participants
+    content = str()
+
+    try:
+        content = fb_m["content"]
+    except Exception:
+        if fb_m["type"] == "Generic":
+            if "photos" in fb_m.keys():
+                content = fb_m["photos"][0]["uri"]
+                fb_m["type"] = "Photo"
+            elif "sticker" in fb_m.keys():
+                content = fb_m["sticker"]["uri"]
+                fb_m["type"] = "Sticker"
+            elif "files" in fb_m.keys():
+                content = fb_m["files"][0]["uri"]
+                fb_m["type"] = "File"
+            elif "gifs" in fb_m.keys():
+                content = fb_m["gifs"][0]["uri"]
+                fb_m["type"] = "Gif"
+            elif "plan" in fb_m.keys():
+                content = fb_m["plan"]["title"]
+                fb_m["type"] = "Plan"
+            elif "videos" in fb_m.keys():
+                content = fb_m["videos"][0]["uri"]
+                fb_m["type"] = "Video"
+            elif "audio_files" in fb_m.keys():
+                content = fb_m["audio_files"][0]["uri"]
+                fb_m["type"] = "Audio"
+            else:
+                print(fb_m)
+
+    message["type"] = fb_m["type"]
+    message["unixTimestamp"] = fb_m["timestamp_ms"]
+    message["sender"] = fb_m["sender_name"]
+    try:
+        message["reactions"] = fb_m["reactions"]
+    except Exception:
+        pass
+
+    message["message"] = content
+    return message
+
+
+def build_message_arr():
+    messages = list()
+    for root, dirs, files in os.walk("./json"):
+        for name in files:
+            if not name.endswith(".json"):
+                continue
+            src_file_path = os.path.join(root, name)
+            convo_name = root.split("\\")[-1]
+
+            with open(src_file_path) as json_file:
                 data = json.load(json_file)
                 print(src_file_path)
-                for m in data['messages']:
-                    message = {}
-                    message['title'] = data['title']
-                    message['participants'] = len(data['participants'])
-                    content = ''
-                    try:
-                        content = m['content']
-                    except Exception as e:
-                        if m['type'] == 'Generic':
-                            try:
-                                content = m['photos'][0]['uri']
-                                m['type'] = 'Photo'
-                            except Exception as e:
-                                try:
-                                    content = m['sticker']['uri']
-                                    m['type'] = 'Sticker'
-                                except Exception as e:
-                                    try:
-                                        content = m['files'][0]['uri']
-                                        m['type'] = 'File'
-                                    except Exception as e:
-                                        try:
-                                            content = m['gifs'][0]['uri']
-                                            m['type'] = 'Gif'
-                                        except Exception as e:
-                                            try:
-                                                content = m['plan']['title']
-                                                m['type'] = 'Plan'
-                                            except Exception as e:
-                                                try:
-                                                    content = m['videos'][0]['uri']
-                                                    m['type']='Video'
-                                                except Exception as e:
-                                                    try:
-                                                        content = m['audio_files'][0]['uri']
-                                                        m['type']='Audio'
-                                                    except Exception as e:
-                                                        print(m)
-                    message['message'] = content
-                    message['type'] = m['type']
-                    message['unixTimestamp'] = m['timestamp_ms']
-                    message['sender'] = m['sender_name']
-                    messages.append(message)
+                for m in data["messages"]:
+                    m_dict = create_message(m, data["title"], len(data["participants"]))
+                    messages.append(m_dict)
+    return messages
 
-with open("messages.json", "w", encoding='utf-8') as text_file:
-    json.dump(messages, text_file) 
-            
+
+def main():
+    with open("messages.json", "w", encoding="utf-8") as text_file:
+        json.dump(build_message_arr(), text_file)
+
+
+if __name__ == "__main__":
+    main()
